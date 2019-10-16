57ac63ae337ea921aa9016b2e9f3ab4d9d16e3bf
Aleksandr Cherepennikov <o-n-i-x@demliz.com>
2019-10-16 19:41:17 +0300

    reformat code

--- src/ViberDriver.php
+++ src/ViberDriver.php
@@ -35,104 +35,104 @@ use TheArdent\Drivers\Viber\Extensions\VideoTemplate;
 
 class ViberDriver extends HttpDriver
 {
-	const DRIVER_NAME = 'Viber';
+    const DRIVER_NAME = 'Viber';
 
-	const API_ENDPOINT = 'https://chatapi.viber.com/pa/';
+    const API_ENDPOINT = 'https://chatapi.viber.com/pa/';
+    
+    /** @var string */
+    protected $signature;
 
-	/** @var string */
-	protected $signature;
+    /** @var  DriverEventInterface */
+    protected $driverEvent;
 
-	/** @var  DriverEventInterface */
-	protected $driverEvent;
+    /** @var string|null */
+    private $botId;
 
-	/** @var string|null */
-	private $botId;
+    /** @var  array|object */
+    private $bot;
 
-	/** @var  array|object */
-	private $bot;
+    /**
+     * @param Request $request
+     */
+    public function buildPayload(Request $request)
+    {
 
-	/**
-	 * @param Request $request
-	 */
-	public function buildPayload(Request $request)
-	{
+        $this->payload = new ParameterBag((array)json_decode($request->getContent(), true));
+        $this->content = $request->getContent();
+        $this->event = Collection::make($this->payload->get('event'), []);
+        $this->signature = $request->headers->get('X-Viber-Content-Signature', '');
+        $this->config = Collection::make($this->config->get('viber'), []);
+    }
 
-		$this->payload = new ParameterBag((array) json_decode($request->getContent(), true));
-		$this->content = $request->getContent();
-		$this->event = Collection::make($this->payload->get('event'), []);
-		$this->signature = $request->headers->get('X-Viber-Content-Signature', '');
-		$this->config = Collection::make($this->config->get('viber'), []);
-	}
+    /**
+     * @return array
+     */
+    protected function getHeaders()
+    {
+        return [
+            'Accept:application/json',
+            'Content-Type:application/json',
+            'X-Viber-Auth-Token: ' . $this->config->get('token'),
+        ];
+    }
 
-	/**
-	 * @return array
-	 */
-	protected function getHeaders()
-	{
-		return [
-			'Accept:application/json',
-			'Content-Type:application/json',
-			'X-Viber-Auth-Token: '.$this->config->get('token'),
-		];
-	}
+    /**
+     * Determine if the request is for this driver.
+     *
+     * @return bool
+     */
+    public function matchesRequest()
+    {
+        return $this->payload->get('event') && $this->payload->get('message_token');
+    }
 
-	/**
-	 * Determine if the request is for this driver.
-	 *
-	 * @return bool
-	 */
-	public function matchesRequest()
-	{
-		return $this->payload->get('event') && $this->payload->get('message_token');
-	}
+    /**
+     * @return bool|DriverEventInterface
+     */
+    public function hasMatchingEvent()
+    {
+        $event = $this->getEventFromEventData($this->payload->all());
+        if ($event) {
+            $this->driverEvent = $event;
+            return $this->driverEvent;
+        }
+        return false;
+    }
 
-	/**
-	 * @return bool|DriverEventInterface
-	 */
-	public function hasMatchingEvent()
-	{
-		$event = $this->getEventFromEventData($this->payload->all());
-		if ($event) {
-			$this->driverEvent = $event;
-			return $this->driverEvent;
-		}
-		return false;
-	}
-
-	/**
-	 * @param array $eventData
-	 *
-	 * @return bool|DriverEventInterface
-	 */
-	public function getEventFromEventData(array $eventData)
-	{
-		switch ($this->event->first()) {
-			case 'delivered':
-				return new MessageDelivered($eventData);
-				break;
-			case 'failed':
-				return new MessageFailed($eventData);
-				break;
-			case 'subscribed':
-				return new UserSubscribed($eventData);
-				break;
-			case 'conversation_started':
-				return new MessageStarted($eventData);
-				break;
-			case 'unsubscribed':
-				return new UserUnsubscribed($eventData);
-				break;
-			case 'seen':
-				return new MessageSeen($eventData);
-				break;
-			case 'webhook':
-				return new Webhook($eventData);
-				break;
-			default:
-				return false;
-				break;
-		}
-	}
+    /**
+     * @param array $eventData
+     *
+     * @return bool|DriverEventInterface
+     */
+    public function getEventFromEventData(array $eventData)
+    {
+        switch ($this->event->first()) {
+            case 'delivered':
+                return new MessageDelivered($eventData);
+                break;
+            case 'failed':
+                return new MessageFailed($eventData);
+                break;
+            case 'subscribed':
+                return new UserSubscribed($eventData);
+                break;
+            case 'conversation_started':
+                return new MessageStarted($eventData);
+                break;
+            case 'unsubscribed':
+                return new UserUnsubscribed($eventData);
+                break;
+            case 'seen':
+                return new MessageSeen($eventData);
+                break;
+            case 'webhook':
+                return new Webhook($eventData);
+                break;
+            default:
+                return false;
+                break;
+        }
+    }
 
     /**
      * @param \BotMan\BotMan\Messages\Incoming\IncomingMessage $message
@@ -146,34 +146,35 @@ class ViberDriver extends HttpDriver
             ->setValue($text);
     }
 
-	/**
-	 * Retrieve the chat message.
-	 *
-	 * @return array
-	 */
-	public function getMessages()
-	{
-		$user = $this->payload->get('sender') ? $this->payload->get('sender')['id'] : ($this->payload->get('user')['id'] ?? null);
-		if ($user === null) {
-			return [];
-		}
-		if (isset($this->payload->get('message')['text'])) {
-			$message = new IncomingMessage($this->payload->get('message')['text'], $user, $this->getBotId(), $this->payload);
-		} elseif ($this->payload->get('message')['type'] == 'location') {
-			$message = new IncomingMessage(Location::PATTERN, $user, $this->getBotId(), $this->payload);
-			$message->setLocation(
-				new Location(
-					$this->payload->get('message')['location']['lat'],
-					$this->payload->get('message')['location']['lon'],
-					$this->payload->get('message')['location']
-				)
-			);
-		} else {
-			$message = new IncomingMessage('', $user, $this->getBotId(), $this->payload);
-		}
+    /**
+     * Retrieve the chat message.
+     *
+     * @return array
+     */
+    public function getMessages()
+    {
+        $user = $this->payload->get('sender') ? $this->payload->get('sender')['id'] : ($this->payload->get('user')['id'] ?? null);
+        if ($user === null) {
+            return [];
+        }
+        if (isset($this->payload->get('message')['text'])) {
+            $message = new IncomingMessage($this->payload->get('message')['text'], $user, $this->getBotId(),
+                $this->payload);
+        } elseif ($this->payload->get('message')['type'] == 'location') {
+            $message = new IncomingMessage(Location::PATTERN, $user, $this->getBotId(), $this->payload);
+            $message->setLocation(
+                new Location(
+                    $this->payload->get('message')['location']['lat'],
+                    $this->payload->get('message')['location']['lon'],
+                    $this->payload->get('message')['location']
+                )
+            );
+        } else {
+            $message = new IncomingMessage('', $user, $this->getBotId(), $this->payload);
+        }
 
-		return [$message];
-	}
+        return [$message];
+    }
 
     /**
      * Convert a Question object
@@ -197,75 +198,79 @@ class ViberDriver extends HttpDriver
             return $keyboard->jsonSerialize();
         }
 
-		return [
-			'text' => $question->getText(),
-			'type' => 'text'
-		];
-	}
+        return [
+            'text' => $question->getText(),
+            'type' => 'text',
+        ];
+    }
 
-	/**
-	 * @param string|Question|IncomingMessage $message
-	 * @param \BotMan\BotMan\Messages\Incoming\IncomingMessage $matchingMessage
-	 * @param array $additionalParameters
-	 * @return array
-	 */
-	public function buildServicePayload($message, $matchingMessage, $additionalParameters = [])
-	{
-		$parameters = array_merge_recursive([
-			'receiver' => $matchingMessage->getSender(),
-		], $additionalParameters);
+    /**
+     * @param string|Question|IncomingMessage                  $message
+     * @param \BotMan\BotMan\Messages\Incoming\IncomingMessage $matchingMessage
+     * @param array                                            $additionalParameters
+     *
+     * @return array
+     */
+    public function buildServicePayload($message, $matchingMessage, $additionalParameters = [])
+    {
+        $parameters = array_merge_recursive([
+            'receiver' => $matchingMessage->getSender(),
+        ], $additionalParameters);
 
-		if ($message instanceof Question) {
-			$parameters = array_merge_recursive($this->convertQuestion($message), $parameters);
-		} elseif ($message instanceof OutgoingMessage) {
-			$attachment = $message->getAttachment();
-			if (! is_null($attachment)) {
-				$attachmentType = strtolower(basename(str_replace('\\', '/', get_class($attachment))));
-				if ($attachmentType == 'image' && $attachment instanceof Image) {
-					$template = new PictureTemplate($attachment->getUrl(), $attachment->getTitle());
-				} elseif ($attachmentType == 'video' && $attachment instanceof Video) {
-					$template = new VideoTemplate($attachment->getUrl());
-				} elseif ($attachmentType == 'audio' && $attachment instanceof Audio) {
-					$template = new FileTemplate($attachment->getUrl(), uniqid().($ext=pathinfo($attachment->getUrl(), PATHINFO_EXTENSION))?'.'.$ext:'');
-				} elseif ($attachmentType == 'file' && $attachment instanceof File) {
-					$template = new FileTemplate($attachment->getUrl(), uniqid().($ext=pathinfo($attachment->getUrl(), PATHINFO_EXTENSION))?'.'.$ext:'');
-				} elseif ($attachmentType == 'location' && $attachment instanceof Location) {
-					$template = new LocationTemplate($attachment->getLatitude(), $attachment->getLongitude());
-				}
+        if ($message instanceof Question) {
+            $parameters = array_merge_recursive($this->convertQuestion($message), $parameters);
+        } elseif ($message instanceof OutgoingMessage) {
+            $attachment = $message->getAttachment();
+            if (!is_null($attachment)) {
+                $attachmentType = strtolower(basename(str_replace('\\', '/', get_class($attachment))));
+                if ($attachmentType == 'image' && $attachment instanceof Image) {
+                    $template = new PictureTemplate($attachment->getUrl(), $attachment->getTitle());
+                } elseif ($attachmentType == 'video' && $attachment instanceof Video) {
+                    $template = new VideoTemplate($attachment->getUrl());
+                } elseif ($attachmentType == 'audio' && $attachment instanceof Audio) {
+                    $template = new FileTemplate($attachment->getUrl(),
+                        uniqid() . ($ext = pathinfo($attachment->getUrl(), PATHINFO_EXTENSION)) ? '.' . $ext : '');
+                } elseif ($attachmentType == 'file' && $attachment instanceof File) {
+                    $template = new FileTemplate($attachment->getUrl(),
+                        uniqid() . ($ext = pathinfo($attachment->getUrl(), PATHINFO_EXTENSION)) ? '.' . $ext : '');
+                } elseif ($attachmentType == 'location' && $attachment instanceof Location) {
+                    $template = new LocationTemplate($attachment->getLatitude(), $attachment->getLongitude());
+                }
 
-				if (isset($template)) {
-					$parameters = array_merge($template->jsonSerialize(), $parameters);
-				}
-			} else {
-				$parameters['text'] = $message->getText();
-				$parameters['type'] = 'text';
-			}
-		} elseif ($message instanceof \JsonSerializable) {
-			$parameters = array_merge($message->jsonSerialize(),$parameters);
-		} else {
-			$parameters['text'] = $message->getText();
-			$parameters['type'] = 'text';
-		}
+                if (isset($template)) {
+                    $parameters = array_merge($template->jsonSerialize(), $parameters);
+                }
+            } else {
+                $parameters['text'] = $message->getText();
+                $parameters['type'] = 'text';
+            }
+        } elseif ($message instanceof \JsonSerializable) {
+            $parameters = array_merge($message->jsonSerialize(), $parameters);
+        } else {
+            $parameters['text'] = $message->getText();
+            $parameters['type'] = 'text';
+        }
 
-		return $parameters;
-	}
+        return $parameters;
+    }
 
-	/**
-	 * @param mixed $payload
-	 * @return Response
-	 */
-	public function sendPayload($payload)
-	{
-		return $this->http->post(self::API_ENDPOINT.'send_message', [], $payload, $this->getHeaders(), true);
-	}
+    /**
+     * @param mixed $payload
+     *
+     * @return Response
+     */
+    public function sendPayload($payload)
+    {
+        return $this->http->post(self::API_ENDPOINT . 'send_message', [], $payload, $this->getHeaders(), true);
+    }
 
-	/**
-	 * @return bool
-	 */
-	public function isConfigured()
-	{
-		return ! is_null($this->config->get('token'));
-	}
+    /**
+     * @return bool
+     */
+    public function isConfigured()
+    {
+        return !is_null($this->config->get('token'));
+    }
 
     /**
      * Retrieve User information.
@@ -289,33 +294,34 @@ class ViberDriver extends HttpDriver
     }
 
 
-	/**
-	 * Low-level method to perform driver specific API requests.
-	 *
-	 * @param string          $endpoint
-	 * @param array           $parameters
-	 * @param IncomingMessage $matchingMessage
-	 *
-	 * @return Response
-	 */
-	public function sendRequest($endpoint, array $parameters, IncomingMessage $matchingMessage)
-	{
-		return $this->http->post(self::API_ENDPOINT.$endpoint, [], $parameters, $this->getHeaders());
-	}
+    /**
+     * Low-level method to perform driver specific API requests.
+     *
+     * @param string          $endpoint
+     * @param array           $parameters
+     * @param IncomingMessage $matchingMessage
+     *
+     * @return Response
+     */
+    public function sendRequest($endpoint, array $parameters, IncomingMessage $matchingMessage)
+    {
+        return $this->http->post(self::API_ENDPOINT . $endpoint, [], $parameters, $this->getHeaders());
+    }
 
-	/**
-	 * Returns the chatbot ID.
-	 * @return string
-	 */
-	private function getBotId()
-	{
-		if (is_null($this->bot)) {
-			$response = $this->http->post(self::API_ENDPOINT.'get_account_info', [], [], $this->getHeaders());
-			$bot = json_decode($response->getContent());
-			$this->bot = $bot;
-			$this->botId = $bot->id;
-		}
+    /**
+     * Returns the chatbot ID.
+     *
+     * @return string
+     */
+    private function getBotId()
+    {
+        if (is_null($this->bot)) {
+            $response = $this->http->post(self::API_ENDPOINT . 'get_account_info', [], [], $this->getHeaders());
+            $bot = json_decode($response->getContent());
+            $this->bot = $bot;
+            $this->botId = $bot->id;
+        }
 
-		return $this->botId;
-	}
+        return $this->botId;
+    }
 }
